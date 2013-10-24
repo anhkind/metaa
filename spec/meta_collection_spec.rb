8c4fee0e4c1b3c630647f726d8d18466dc645a31
anguyen <anguyen@elctech.com>
2013-10-24 22:17:06 +0800

    Updated tests

--- spec/meta_collection_spec.rb
+++ spec/meta_collection_spec.rb
@@ -4,22 +4,22 @@ describe Metaa::MetaCollection do
   describe '#add' do
     it 'add a meta object to internal list' do
       meta_collection = Metaa::MetaCollection.new
-      meta_collection.add Metaa::Meta.new({name: 'keywords', content: 'abc, xyz'})
-      expect(meta_collection.items.first.attributes).to eq({name: 'keywords', content: 'abc, xyz'})
+      meta_collection.add Metaa::Meta.new({:name => 'keywords', :content => 'abc, xyz'})
+      expect(meta_collection.items.first.attributes).to eq({:name => 'keywords', :content => 'abc, xyz'})
     end
 
     it 'add a hash as a new meta object to internal list' do
       meta_collection = Metaa::MetaCollection.new
-      meta_collection.add({name: 'keywords', content: 'abc, xyz'})
-      expect(meta_collection.items.first.attributes).to eq({name: 'keywords', content: 'abc, xyz'})
+      meta_collection.add({:name => 'keywords', :content => 'abc, xyz'})
+      expect(meta_collection.items.first.attributes).to eq({:name => 'keywords', :content => 'abc, xyz'})
     end
   end
 
   describe '#html' do
     it 'converts list of meta objects to html' do
       meta_list = [
-        Metaa::Meta.new({name: 'keywords', content: 'abc, xyz'}),
-        Metaa::Meta.new({name: 'keywords', content: '123, 456'})
+        Metaa::Meta.new({:name => 'keywords', :content => 'abc, xyz'}),
+        Metaa::Meta.new({:name => 'keywords', :content => '123, 456'})
       ]
       meta_collection = Metaa::MetaCollection.new(meta_list)
       expect(meta_collection.html).to eq "<meta content=\"abc, xyz\" name=\"keywords\" /><meta content=\"123, 456\" name=\"keywords\" />"
--- spec/meta_spec.rb
+++ spec/meta_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Metaa::Meta do
   it 'converts a meta object to html' do
-    meta      = Metaa::Meta.new({name: 'keywords', content: 'abc, xyz'})
+    meta      = Metaa::Meta.new({:name => 'keywords', :content => 'abc, xyz'})
     expect(meta.html).to eq "<meta content=\"abc, xyz\" name=\"keywords\" />"
   end
 end
